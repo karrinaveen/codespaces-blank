ae31731495b334326cef0ca3901fd0ff42c3095e
Naveen Karri <45682542+karrinaveen@users.noreply.github.com>
2023-06-28 12:53:19 +0000

    default dict added to reduce stack size

--- Data structures/tree_height/tree_height.py
+++ Data structures/tree_height/tree_height.py
@@ -2,6 +2,8 @@
 
 import sys
 import threading
+import time
+from collections import defaultdict
 
 class node():
     def __init__(self,value):
@@ -24,8 +26,9 @@ def compute_height_naive(n, parents):
         max_height = max(max_height, height)
     return max_height
 
-def compute_height(n,parents):
+def compute_height_recursion(n,parents):
     nodes = [node(i) for i in range(n)]
+    root = 0
 
     for child,parent in enumerate(parents):
         if parent==-1:
@@ -42,13 +45,41 @@ def compute_height(n,parents):
             return height
 
     return max_height(nodes[root])
+
+def compute_height(n,parents):
+    nodes = [node(i) for i in range(n)]
+    root = 0
+    for child,parent in enumerate(parents):
+        if parent==-1:
+            root = child
+        else:
+            nodes[parent].add_child(nodes[child])
     
+    heights = defaultdict(int)
+    def height(node):
+        if node.value==root:
+            return 1
+        else:
+            pnode = nodes[parents[node.value]]
+            if heights[pnode.value]!=0:
+                heights[node.value] = 1+ heights[pnode.value]
+            else:
+                heights[node.value] = 1+ height(pnode)
+        return heights[node.value]
+    
+    max_height = 1
+    for n in nodes:
+        if len(n.child)==0:
+            max_height = max(max_height,height(n))
+    
+    return max_height
 
 
 def main():
     n = int(input())
     parents = list(map(int, input().split()))
     print(compute_height(n, parents))
+
 
 
 # In Python, the default limit on recursion depth is rather low,
