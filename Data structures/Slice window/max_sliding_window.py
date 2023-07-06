69251d9459caba53894dfa76e99f7b26b7dcc194
Naveen Karri <45682542+karrinaveen@users.noreply.github.com>
2023-07-06 09:37:30 +0000

    minor edit

--- Data structures/Slice window/max_sliding_window.py
+++ Data structures/Slice window/max_sliding_window.py
@@ -15,15 +15,19 @@ def max_sliding_window_approach3(sequence,m):
         slider.popleft()
         left_ele = sequence[i-m+1]
         right_ele = sequence[i]
-        newslider = deque()
-        for ele in slider:
-            if ele >= right_ele:
-                newslider.append(ele)
-        if len(newslider)==0 or newslider[0]!=left_ele:
-            newslider.appendleft(left_ele)
-        newslider.append(right_ele)
-        slider = newslider.copy()
-        maximums.append(max(slider))
+        if right_ele>=maximums[-1]:
+            slider.append(right_ele)
+            maximums.append(right_ele)
+        else:
+            newslider = deque()
+            for ele in slider:
+                if ele >= right_ele:
+                    newslider.append(ele)
+            if len(newslider)==0 or newslider[0]!=left_ele:
+                newslider.appendleft(left_ele)
+            newslider.append(right_ele)
+            slider = newslider.copy()
+            maximums.append(max(slider))
     return maximums
 
 
